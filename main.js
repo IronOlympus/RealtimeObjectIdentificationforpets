0483509d32c266d5c7c28923996c7a0ddc06f4d1
IronOlympus <85447860+IronOlympus@users.noreply.github.com>
2022-08-14 15:12:39 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 function setup(){
     canvas = createCanvas(200,200);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function draw(){
+    image(video,0,0,200,200);
+}
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Detecting Objects";
+}
+function modelLoaded(){
+    console.log("Model Loaded");
+    status1 = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
 }
