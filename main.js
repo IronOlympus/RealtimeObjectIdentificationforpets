3b22f47659c592c40e7fde9275049be8bec341a7
IronOlympus <85447860+IronOlympus@users.noreply.github.com>
2022-08-21 15:47:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+var status1 = "";
+var objects = [];
+video = "";
+var lookfor = document.getElementById("input").value;
 function setup(){
     canvas = createCanvas(200,200);
     canvas.center();
@@ -6,6 +10,30 @@ function setup(){
 }
 function draw(){
     image(video,0,0,200,200);
+    if(status1 != ""){
+        objectDetector.detect(video, gotResult);
+        for(i = 0; i < objects.length; i++){
+           document.getElementById("status").innerHTML = "Objects Detected";
+           document.getElementById("number_of_obj").innerHTML = "Number of Objects : " + objects.length;
+           fill('#FF0000');
+           var percent = floor(objects[i].confidence*100);
+           text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+           noFill();
+           stroke('#FF0000');
+           rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+           console.log(objects[i].label);
+           var lookfor = document.getElementById("input").value;
+           if(lookfor == objects[i].label){
+            var synth = window.speechSynthesis;
+            speak_data_1 = lookfor + " detected!";
+            var utterThis = new SpeechSynthesisUtterance(speak_data_1);
+            synth.speak(utterThis);
+             window.alert(lookfor + " detected!");
+             objectDetector.detect(gotResult);
+             webcamLiveView.stop();
+           }
+        }
+    }
 }
 function start(){
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
@@ -17,4 +45,13 @@ function modelLoaded(){
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        objects = results;
+    }
 }
